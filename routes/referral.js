83362c0d801a50e19cd1c21162e181a01f3be3b3
hades255 <hades.255@outlook.com>
2024-08-09 06:29:49 -0400

    frens

--- routes/referral.js
+++ routes/referral.js
@@ -1,5 +1,28 @@
 const express = require("express");
+const User = require("../models/User");
+const Referral = require("../models/Referral");
 
 const router = express.Router();
+
+router.get("/frens", async (req, res) => {
+  const { userId } = req.query;
+  try {
+    let userIds = await Referral.distinct("userId", {
+      $or: [{ userId }, { code: userId }],
+    }).exec();
+    if (!userIds.includes(userId)) {
+      userIds = [...userIds, userId];
+    }
+    const frens = await User.find({ chatId: { $in: userIds } })
+      .sort({
+        point: -1,
+      })
+      .limit(100);
+    res.json({ msg: "ok", frens });
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error.message);
+  }
+});
 
 module.exports = router;
