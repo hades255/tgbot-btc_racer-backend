1a941fdb07d8932bed3019d494925bb4e1711b9c
hades255 <hades.255@outlook.com>
2024-08-20 05:38:49 -0400

    referral code

--- routes/bot.js
+++ routes/bot.js
@@ -1,50 +1,73 @@
 const TelegramBot = require("node-telegram-bot-api");
 const Referral = require("../models/Referral");
+const User = require("../models/User");
 
 const token = "7067970345:AAFs9OaXzqCWMK4h85WAujH80d8C0_AFZSI";
 const bot = new TelegramBot(token, { polling: true });
+
 bot.onText(/\/start(?: (.+))?/, async (msg, match) => {
-  const chatId = msg.chat.id;
-  const referralCode = match[1];
-  let reftext = "Welcome! No referral code found.";
-  if (referralCode) {
-    reftext = `Referral Code: ${referralCode}`;
-    saveReferralCode(chatId, referralCode);
-  }
-  const {
-    username = "",
-    last_name = "",
-    first_name = "",
-  } = await bot.getChat(chatId);
-  bot.sendMessage(
-    chatId,
-    `${reftext}\nWelcome! Click the button below to start app.`,
-    {
-      reply_markup: {
-        inline_keyboard: [
-          [
-            {
-              text: "GoApp",
-              web_app: {
-                url: `https://efc2-172-86-113-74.ngrok-free.app?userId=${chatId}&username=${username}&name=${
-                  first_name + " " + last_name
-                }`,
-              },
-            },
-          ],
-        ],
-      },
+  try {
+    const chatId = msg.chat.id;
+    const referralCode = match[1];
+    let reftext = "Welcome! No referral code found.";
+    let bonus = "";
+    if (referralCode) {
+      reftext = `Referral Code: ${referralCode}`;
+      bonus = await saveReferralCode(chatId, referralCode);
     }
-  );
+    const {
+      username = "",
+      last_name = "",
+      first_name = "",
+    } = await bot.getChat(chatId);
+    bot.sendMessage(
+      chatId,
+      `${reftext}\nWelcome! Click the button below to start app.`,
+      {
+        reply_markup: {
+          inline_keyboard: [
+            [
+              {
+                text: "GoApp",
+                web_app: {
+                  url: `https://efc2-172-86-113-74.ngrok-free.app?userId=${chatId}&username=${username}&name=${
+                    first_name + " " + last_name
+                  }&refer=${referralCode}&bonus=${bonus}`,
+                },
+              },
+            ],
+          ],
+        },
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-const saveReferralCode = (userId, referralCode) => {
-  new Referral({
-    code: referralCode,
-    userId: userId,
-    timestamp: new Date(),
-  })
-    .save()
-    .then(() => console.log("Referral Saved"))
-    .catch((err) => console.error(err));
+const saveReferralCode = async (userId, referralCode) => {
+  try {
+    const referrer = await User.findOne({ chatId: referralCode });
+    if (referrer) {
+      const oldref = await Referral.findOne({
+        code: referralCode,
+        userId: userId,
+      });
+      if (oldref) return null;
+      const user = await User.findOne({ chatId: userId });
+      const bonus = referrer.point / 10 > 10000 ? 10000 : referrer.point / 10;
+      user.point = user.point + 5000 + bonus;
+      await user.save();
+      await new Referral({
+        code: referralCode,
+        userId: userId,
+        timestamp: new Date(),
+      }).save();
+      return bonus;
+    }
+    return null;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 };
