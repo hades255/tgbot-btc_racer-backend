547778cc18f6b5f66ea09faf0d5537269a6762f0
hades255 <hades.255@outlook.com>
2024-09-03 11:17:47 -0400

    referral issue

--- routes/referral.js
+++ routes/referral.js
@@ -7,13 +7,23 @@ const router = express.Router();
 router.get("/frens", async (req, res) => {
   const { userId } = req.query;
   try {
-    let userIds = await Referral.distinct("userId", {
-      code: userId,
-      read: true,
+    let userIds1 = await Referral.distinct("userId", {
+      $or: [{ userId }, { code: userId }],
     }).exec();
+    let userIds2 = await Referral.distinct("code", {
+      $or: [{ userId }, { code: userId }],
+    }).exec();
+    const userIds = [userId, ...userIds1]
+      .concat(userIds2)
+      .reduce((acc, item) => {
+        if (!acc.includes(item)) {
+          acc.push(item);
+        }
+        return acc;
+      }, []);
 
     const frens = await User.find({
-      chatId: { $in: [...userIds, userId] },
+      chatId: { $in: userIds },
     }).sort({
       point: -1,
     });
